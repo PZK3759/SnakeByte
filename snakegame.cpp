b03fe4bf71e0c4123cf511fbaf2da3e50bfe4add
Zahin <Zaheen.Kenji@gmail.com>
2025-12-14 01:43:59 +0600

    updated gitignore

--- snakegame.cpp
+++ snakegame.cpp
@@ -76,10 +76,10 @@ void SnakeGame::initializeLevels() {
     std::string bg1 = "assets/images/level1_bg.png";
     std::string bg2 = "assets/images/levelbg_2.png";
     
-    Level level1(1, 70, "Level 1", music1, bg2, Color(0, 255, 0), 4.0f, true);
+    Level level1(1, 30, "Level 1", music1, bg2, Color(0, 255, 0), 4.0f, true);
     levels.push_back(level1);
     
-    Level level2(2, 150, "Level 2", music2, bg2, Color(255, 255, 0), 4.0f, true);
+    Level level2(2, 60, "Level 2", music2, bg2, Color(255, 255, 0), 4.0f, true);
     level2.setupDefaultObstacles(PLAYFIELD_START_ROW);
     levels.push_back(level2);
     
@@ -587,7 +587,7 @@ void SnakeGame::moveSnake() {
         updateMultipliers();
     
         if (!isFreePlay) {
-        speedMultiplier += 0.1f;
+            speedMultiplier += 0.1f;
         }
     
         spawnFood(NORMAL);
@@ -802,7 +802,7 @@ void SnakeGame::render() {
     
     if (state == MAIN_MENU) {
         renderMainMenu();
-    } else if (state == LEVEL_SELECT) {  // NEW
+    } else if (state == LEVEL_SELECT) { 
         renderLevelSelect();
     } else if (state == PLAYING || state == PAUSED) {
         renderGame();
